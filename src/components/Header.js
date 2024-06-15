c1547b654d2970e63af6a3657bf1480fd8e0df6c
Abhishekr2122 <abhishekrshinde21@gmail.com>
2024-06-15 12:45:38 +0530

    Building header component.

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import "./header.css";
+import HeaderLogo from "../ui/HeaderLogo";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <HeaderLogo />
+    </header>
+  );
+}
---
+++ src/ui/HeaderLogo.js
@@ -0,0 +1,12 @@
+import "./headerlogo.css";
+
+export default function HeaderLogo() {
+  return (
+    <div className="logo-container">
+      <div>
+        <img src="/profile-1.jpeg" alt="no-data" className="image" />
+      </div>
+      <p className="para">Abhishek Shinde</p>
+    </div>
+  );
+}
