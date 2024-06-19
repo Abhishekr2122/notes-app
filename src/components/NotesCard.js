afeb409918521472f866ca180742ee3c03058b10
Abhishekr2122 <abhishekrshinde21@gmail.com>
2024-06-19 17:23:11 +0530

    Working on the notes-card component.

--- src/components/NotesCard.js
+++ src/components/NotesCard.js
@@ -3,17 +3,28 @@ import "./notescard.css";
 export default function NotesCard() {
   return (
     <article className="notes-card">
-      <div>{/* <img src="/notes-icon-1.jpeg.jfif" alt="no-data" /> */}</div>
-      <div>
-        <h1>Title</h1>
-        <h2>Sub-title</h2>
-        <p>content</p>
-        <div>
-          <div>
+      <div className="image-container">
+        <div className="card-image-container">
+          <img
+            src="/notes-icon-2.jpeg.jfif"
+            alt="no-data"
+            className="notes-image"
+          />
+        </div>
+      </div>
+      <div className="notes-main-content-container">
+        <div className="notes-main-content">
+          <h1 className="notes-title">Title</h1>
+          <h2 className="notes-sub-title">Sub-title</h2>
+          <p className="notes-paragraph">content</p>
+        </div>
+
+        <div className="notes-sub-content-container">
+          <div className="date-time-container">
             <p>date</p>
             <p>time</p>
           </div>
-          <div>pinned favourites saved</div>
+          <div className="options-icon-container">pinned favourites saved</div>
         </div>
       </div>
     </article>
